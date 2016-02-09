414a51c006dae0a43895ace366c0b41c7710d183
Morgan Pretty <morgan@homepass.com>
2016-02-09 11:34:57 +1100

    Added support for colours with alpha values.

--- ios/RNSpinkit.m
+++ ios/RNSpinkit.m
@@ -81,6 +81,10 @@
    NSScanner *scanner = [NSScanner scannerWithString:color];
    [scanner setScanLocation:1]; // bypass '#' character
    [scanner scanHexInt:&rgbValue];
+    
+   if (color.length == @"#00000000".length) {
+      return [UIColor colorWithRed:((rgbValue & 0xFF000000) >> 24)/255.0 green:((rgbValue & 0xFF0000) >> 16)/255.0 blue:((rgbValue & 0xFF00) >> 8)/255.0 alpha:(rgbValue & 0xFF)/255.0];
+   }
    
    return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:1.0];
 }
