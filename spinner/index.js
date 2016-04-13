57f39d0dbf1e87df59dc52a73358cc4040a031cf
NishanthShankar <nishanth@lensbricks.com>
2016-04-12 17:15:27 -0700

    Add 9CubeGrid support for android

--- spinner/index.js
+++ spinner/index.js
@@ -1,7 +1,9 @@
 var React = require('react-native');
 
 var types = {
-	'ThreeBounce': require('./three-bounce')
+	'ThreeBounce': require('./three-bounce'),
+	'9CubeGrid':require('./nine-cube-grid'),
+	'Wave': require('./wave')
 };
 
 class Spinner extends React.Component {
