96fba4435c690fe8d79389368dd02723f108cc3d
Max <maxs15@users.noreply.github.com>
2016-05-25 17:13:07 -0400

    Fix rnpm prelink script SyntaxError

--- scripts/rnpm-prelink.js
+++ scripts/rnpm-prelink.js
@@ -10,10 +10,10 @@ try {
   console.log('Failed to load `android/build.gralde` when linking react-native-spinkit');
 }
 
-var depStr = String(cfg).match(/allprojects(.|[\r\n])+/)
+var depStr = String(cfg).match(/allprojects(.|[\r\n])+/);
 
 if(depStr === null) {
-  console.log('Could not find `allprojects { }` block in build.gradle')
+  console.log('Could not find `allprojects { }` block in build.gradle');
 }
 
 // search fro allprojects {...} block
@@ -37,13 +37,13 @@ for(var i in str) {
 // add jitpack repo to `repositories` block
 var dep = str.substr(0, replacePos);
 
-// terminate if the repository already exists
-if(String(dep).match(/url[^h]*https\:\/\/jitpack\.io/) !== null)
-return ;
+// Chech if the repository already exists
+if (String(dep).match(/url[^h]*https\:\/\/jitpack\.io/) === null) {
 
-dep = String(dep).replace(/repositories[^\{]*\{/, 'repositories {\r\n\t\t\t\t// Add jitpack repository (added by react-native-spinkit)\r\n\t\t\t\tmaven { url "https://jitpack.io" }');
-str = dep + str.substr(replacePos, str.length - replacePos);
+  dep = String(dep).replace(/repositories[^\{]*\{/, 'repositories {\r\n\t\t\t\t// Add jitpack repository (added by react-native-spinkit)\r\n\t\t\t\tmaven { url "https://jitpack.io" }');
+  str = dep + str.substr(replacePos, str.length - replacePos);
 
-// replace original build script
-depStr = String(cfg).replace(/allprojects(.|[\r\n])+/, str);
-fs.writeFileSync(GRADLE_SCRIPT_PATH, depStr);
+  // replace original build script
+  depStr = String(cfg).replace(/allprojects(.|[\r\n])+/, str);
+  fs.writeFileSync(GRADLE_SCRIPT_PATH, depStr);
+}
