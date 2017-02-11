d52d9efb86b4a1160dea43484f801b11c75c40af
nickburkhart <nickburkhart@gmail.com>
2017-02-11 13:46:54 -0500

    spelling

--- scripts/rnpm-prelink.js
+++ scripts/rnpm-prelink.js
@@ -7,7 +7,7 @@ try {
   var cfg = fs.readFileSync(GRADLE_SCRIPT_PATH);
 } catch(err) {
   console.log(err.stack);
-  console.log('Failed to load `android/build.gralde` when linking react-native-spinkit');
+  console.log('Failed to load `android/build.gradle` when linking react-native-spinkit');
 }
 
 var depStr = String(cfg).match(/allprojects(.|[\r\n])+/);
