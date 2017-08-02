7326cc9104a28aa28dd47746368a6b081ff4d38d
John <jgrant.ber@gmail.com>
2017-08-01 17:04:34 -0700

    Update RNSpinkitPackage.java

--- android/src/main/java/com/react/rnspinkit/RNSpinkitPackage.java
+++ android/src/main/java/com/react/rnspinkit/RNSpinkitPackage.java
@@ -21,11 +21,6 @@ public class RNSpinkitPackage implements ReactPackage{
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         List<ViewManager> list = new ArrayList<>();
         list.add(new RNSpinkit(reactContext));
